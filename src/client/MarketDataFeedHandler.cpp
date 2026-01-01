882a3f6590d8acb06cee518bb3da01c0e66cbdeb
Laurynas Notrumas <laurynas.notrumas@gmail.com>
2026-01-01 13:10:41 +0200

    Structure files into directories

--- src/client/MarketDataFeedHandler.cpp
+++ src/client/MarketDataFeedHandler.cpp
@@ -6,55 +6,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#pragma pack(push, 1)
-
-struct ItchHeader
-{
-    uint64_t sequenceNumber;
-    char messageType;
-    uint64_t timestamp;
-};
-
-struct OrderAddMsg
-{
-    ItchHeader header;
-    uint64_t orderId;
-    uint8_t side;
-    uint32_t quantity;
-    char symbol[4];
-    uint32_t price;
-};
-
-struct OrderExecMsg
-{
-    ItchHeader header;
-    uint64_t orderId;
-    uint32_t quantity;
-    uint64_t matchId;
-};
-
-struct OrderDeleteMsg
-{
-    ItchHeader header;
-    uint64_t orderId;
-};
-
-struct TradeMsg
-{
-    ItchHeader header;
-    uint8_t side;
-    uint32_t quantity;
-    char symbol[4];
-    uint32_t price;
-    uint64_t matchId;
-};
-
-struct EndMarketMsg
-{
-    ItchHeader header;
-};
-
-#pragma pack(pop)
+#include "ItchMessage.hpp"
 
 constexpr size_t MAX_PACKET_SIZE = 128;
 
--- src/exchange/UDPTransmitter.hpp
+++ src/exchange/UDPTransmitter.hpp
@@ -1,61 +1,15 @@
 #pragma once
 
+#include <iostream>
+#include <cstring>
+
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 
-#include <cstring>
-
-#pragma pack(push, 1)
-
-struct ItchHeader
-{
-    uint64_t sequenceNumber;
-    char messageType;
-    uint64_t timestamp;
-};
-
-struct OrderAddMsg
-{
-    ItchHeader header;
-    uint64_t orderId;
-    uint8_t side;
-    uint32_t quantity;
-    char symbol[4];
-    uint32_t price;
-};
-
-struct OrderExecMsg
-{
-    ItchHeader header;
-    uint64_t orderId;
-    uint32_t quantity;
-    uint64_t matchId;
-};
-
-struct OrderDeleteMsg
-{
-    ItchHeader header;
-    uint64_t orderId;
-};
-
-struct TradeMsg
-{
-    ItchHeader header;
-    uint8_t side;
-    uint32_t quantity;
-    char symbol[4];
-    uint32_t price;
-    uint64_t matchId;
-};
-
-struct EndMarketMsg
-{
-    ItchHeader header;
-};
-
-#pragma pack(pop)
+#include "ItchMessage.hpp"
+#include "Timer.hpp"
 
 class UDPTransmitter
 {
